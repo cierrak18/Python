68d940d9136b04e08d6f314c3a8e9aff391fb545
Cierra <ibritt.cierra@gmail.com>
2025-11-02 00:27:34 -0400

    Delete Hacker Rank directory

--- Hacker Rank/Readme.md
+++
@@ -1,2 +0,0 @@
-# Introduction
-Within this folder, you'll find practice problems I've tackled on HackerRank, demonstrating my evolving proficiency in Python. - Cierra B.
--- Hacker Rank/hello2.py
+++
@@ -1,7 +0,0 @@
-if __name__ == '__main__':
-    n = int(input())
-    arr = map(int, input().split())
-    list2 = list(set(arr))
-    list2.sort()
-    secondbest =list2[-2]
-print(secondbest)
